978f2e4ed0c93ebfd737871b368794431ec1ff3a
zeenkeen <zeenalam5@gmail.com>
2024-09-17 12:08:40 +0530

    not much

--- dataTypes.js
+++ dataTypes.js
@@ -8,14 +8,42 @@
 
 // object
 
-let name = "zeen"
-let age = "28"
-let is_logged_in = false
+// let name = "zeen"
+// let age = "28"
+// let is_logged_in = false
 
-console.log(typeof name)
-console.log(typeof age)
+// console.log(typeof name)
+// console.log(typeof age)
 
-console.log(typeof null)
-console.log(typeof true)
+// console.log(typeof null)
+// console.log(typeof true)
 
-console.log(typeof "2234ad")
+// console.log(typeof "2234ad")
+
+
+let score = "33"
+
+console.log(typeof score)
+
+let valuinNUmber = Number(score)
+console.log(typeof valuinNUmber)
+
+// let understand a cse of NaN- not a number
+
+let value = "33abd"
+console.log(typeof value)
+
+let valueNumber = Number(value)
+console.log(typeof valueNumber)
+
+// let's see why valuNumber is being shown as number data type
+console.log(valueNumber) //but in reality it is a NaN
+
+// "33" => 33
+// "33abd" => NaN
+// true => 1: false => 0
+
+/* boolean conversion */
+//1 => true; 0 => false
+// "" => false
+// "zeen" => true
