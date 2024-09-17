771789e6e8f1a340925eb85648db61c53472d036
zeenkeen <zeenalam5@gmail.com>
2024-09-17 11:16:06 +0530

    datatyps

---
+++ dataTypes.js
@@ -0,0 +1,21 @@
+// some common data types in Js
+// number => 2 to power 53
+// binint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+
+// object
+
+let name = "zeen"
+let age = "28"
+let is_logged_in = false
+
+console.log(typeof name)
+console.log(typeof age)
+
+console.log(typeof null)
+console.log(typeof true)
+
+console.log(typeof "2234ad")
