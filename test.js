ef221259b1aa4c183088a11e2f29e862fb7bbc35
zeenkeen <zeenalam5@gmail.com>
2024-09-17 09:16:17 +0530

    done with variables

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log("zeen")
+const account_id = 1
+let account_email = "zeen@gmail.com"
+var account_pass = "123456"
+account_city = "Jaipur"
+let account_state;
+
+// account_id = 2
+
+console.table([account_id,account_email,account_pass,account_city,account_state]);
+/* we will strictly use let to declare variables and nothing else
+because of the issue in functional scope and block scope */
