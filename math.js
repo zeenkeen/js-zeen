e1c9d55503e08fe88e5e02c4ba7eeca9c89814cb
zeenkeen <zeenalam5@gmail.com>
2024-09-18 11:49:36 +0530

    played with some math

---
+++ math.js
@@ -0,0 +1,16 @@
+//absolute value
+console.log(Math.abs(-123515)) //converts any number to its absolut form that positive number
+console.log(typeof Math.abs)// it return Number and not string
+
+const negativeNUm = -145
+const absoluteNum = Math.abs(negativeNUm)
+console.log(typeof absoluteNum, absoluteNum)
+console.log(absoluteNum)
+
+console.log(Math.round(4.23462))
+
+console.log(Math.ceil(4.18765432)) //it will always return 5
+console.log(Math.floor(2362.272)) // it will always return 2362
+ 
+console.log(Math. min(1,16,57,843232,1226,)) //return the minimum value
+console.log(Math.max(125,34623457,124632,5235,737124363,51,436,)) //returns the max value
