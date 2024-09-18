c3882e3ecd5a2f3897a84a5c93a902391a9044d2
zeenkeen <zeenalam5@gmail.com>
2024-09-18 11:07:43 +0530

    nothing

---
+++ str_interpolatin.js
@@ -0,0 +1,14 @@
+const name = "zeen"
+const repoCount = 4
+
+// console.log(name + repoCount + " value")
+
+// similar to f in python we have here also
+console.log(`Hello! my name is ${name} and my repo count is ${repoCount}`)
+
+const gameNmae = new String("zeen.alam")
+console.log(gameNmae[0])
+console.log(gameNmae.__proto__)
+
+console.log(gameNmae.length)
+console.log(gameNmae.toUpperCase())
